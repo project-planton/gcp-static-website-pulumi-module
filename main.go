bcbfce05e0ee128911352b1c49042bd6eaf14ae1
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-30 21:30:24 +0530

    using project-planton apis

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	gcpstaticwebsitev1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/apis/provider/gcp/gcpstaticwebsite/v1"
 	"github.com/pkg/errors"
 	"github.com/plantoncloud/gcp-static-website-pulumi-module/pkg"
-	"github.com/plantoncloud/project-planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/gcp/gcpstaticwebsite"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		stackInput := &gcpstaticwebsite.GcpStaticWebsiteStackInput{}
+		stackInput := &gcpstaticwebsitev1.GcpStaticWebsiteStackInput{}
 
 		if err := stackinput.LoadStackInput(ctx, stackInput); err != nil {
 			return errors.Wrap(err, "failed to load stack-input")
--- pkg/resources.go
+++ pkg/resources.go
@@ -1,13 +1,13 @@
 package pkg
 
 import (
+	gcpstaticwebsitev1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/apis/provider/gcp/gcpstaticwebsite/v1"
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/project-planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/gcp/gcpstaticwebsite"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/provider/gcp/pulumigoogleprovider"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func Resources(ctx *pulumi.Context, stackInput *gcpstaticwebsite.GcpStaticWebsiteStackInput) error {
+func Resources(ctx *pulumi.Context, stackInput *gcpstaticwebsitev1.GcpStaticWebsiteStackInput) error {
 	//create gcp provider using the credentials from the input
 	_, err := pulumigoogleprovider.Get(ctx, stackInput.GcpCredential)
 	if err != nil {
