0884657de45f3154df4e969c007001e1ae75b354
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-10-12 16:52:46 +0530

    moved to project-planton

--- pkg/resources.go
+++ pkg/resources.go
@@ -1,9 +1,9 @@
 package pkg
 
 import (
-	gcpstaticwebsitev1 "buf.build/gen/go/plantoncloud/project-planton/protocolbuffers/go/project/planton/provider/gcp/gcpstaticwebsite/v1"
+	gcpstaticwebsitev1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/gcp/gcpstaticwebsite/v1"
 	"github.com/pkg/errors"
-	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/provider/gcp/pulumigoogleprovider"
+	"github.com/project-planton/pulumi-module-golang-commons/pkg/provider/gcp/pulumigoogleprovider"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
